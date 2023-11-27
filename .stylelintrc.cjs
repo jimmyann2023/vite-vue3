d94c2c250e25348eb819ae200d48b5b45b523874
jimmyann2023 <2492617236@qq.com>
2023-11-27 16:01:19 +0800

    🐛 fix:(custom): add stylelint

---
+++ .stylelintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+    'stylelint-config-html/vue',
+    'stylelint-config-recommended-vue/scss',
+    'stylelint-config-recommended-less',
+    'stylelint-config-recommended-scss',
+  ],
+  plugins: ['stylelint-order'],
+  overrides: [
+    {
+      files: ['**/*.vue'],
+      customSyntax: 'postcss-html',
+    },
+  ],
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.json', '**/*.md'],
+  rules: {
+    // null =》关闭规则
+    // always =》必须
+    indentation: 2,
+    'selector-pseudo-element-no-unknow': [
+      // 不允许未知的选择器
+      true,
+      {
+        ignorePseudoElements: ['global', 'v-deep', ':deep', 'deep'], // 忽略属性，修改antd 默认样式的时候能使用到
+      },
+    ],
+  },
+};
--- doc/git-husky.md
+++ doc/git-husky.md
@@ -1,3 +1,23 @@
 https://www.nowcoder.com/discuss/522942631710588928
 
 https://zhuanlan.zhihu.com/p/619030608?utm_id=0
+
+第一步 添加要检查上传的文件
+
+```bash
+pnpm git:add
+```
+
+第二步 暂存区文件检查 并且提交信息
+
+```bash
+pnpm commit
+```
+
+第三步 提交远程分支
+
+```bash
+git push origin main #git远程分支
+
+git push gitlab main #gitlab 远程分支
+```
